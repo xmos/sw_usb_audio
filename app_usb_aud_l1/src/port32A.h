64c7cccb4662506bf193d7c1767f1acdbcf84f1b
Ross Owen <ross@xmos.com>
2012-03-05 12:42:14 +0000

    Adding app_usb_aud_l1 files

---
+++ app_usb_aud_l1/src/port32A.h
@@ -0,0 +1,34 @@
+/**
+ * @file        port32A.h
+ * @brief       Port 32A access functions and bit defs for L1 USB Audio board 
+ * @author      Ross Owen, XMOS Semiconductor 
+ * @version     1.0
+ * @date        30/01/2010
+ */
+
+#ifndef _PORT32A_OUT_
+#define _PORT32A_OUT_ 
+
+#ifdef BOARD_S1
+/* General output port bit definitions */
+#define P32A_COD_RST_N     0x01    /* CODEC reset. Active low. */
+#define P32A_MCLK_SEL      0x02    /* MCLK frequency select. 0 - 22.5792MHz, 1 - 24.576MHz */
+#define P32A_PWR_EN_N      0x04    /* +5V Power Enable. Active low. */
+#define P32A_LED_A         0x08    /* LED A. Active high */
+#define P32A_LED_B         0x10    /* LED B. Active high */
+#else
+/* Bit defs */
+#define P32A_USB_RST       0x01           /* ULPI rst line */  
+#define P32A_COD_RST       0x02           /* Codec rst line */
+#define P32A_CLK_SEL       0x04           /* MClk Select line */
+#define P32A_LED_A         0x08           /* LED A */
+#define P32A_LED_B         0x10           /* LED B */
+#endif
+
+/* Outputs passed value to port 32A */
+void port32A_out(int value);
+
+/* Performs peek operation on port 32A and returns value on pins */
+unsigned port32A_peek();
+
+#endif
