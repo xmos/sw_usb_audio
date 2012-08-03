c5414acb74e9bd619364484dbd60d757e59468e1
Ross Owen <ross@xmos.com>
2012-08-03 12:19:40 +0100

    Makefile updates and removed HID

--- app_usb_aud_su1/src/config_spdif/customdefines.h
+++ app_usb_aud_su1/src/config_spdif/customdefines.h
@@ -30,8 +30,6 @@
 /* Run the CODEC as slave, Xcore as master */
 #define CODEC_SLAVE     1
 
-#define HID_CONTROLS    1
-
 /* Enable DFU interface, Note, requires a driver for Windows */
 #define DFU             1
 
