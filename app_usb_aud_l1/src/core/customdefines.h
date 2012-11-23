75f7ad9cffa09451ddcf6fee9f4e1d20109ced42
Ross Owen <ross@xmos.com>
2012-11-23 17:01:14 +0000

    Updted build configs for L1

--- app_usb_aud_l1/src/core/customdefines.h
+++ app_usb_aud_l1/src/core/customdefines.h
@@ -7,65 +7,86 @@
 #ifndef _CUSTOMDEFINES_H_
 #define _CUSTOMDEFINES_H_
 
-#define HID_CONTROLS 1
 
-/***** Device configuration option defines.  Build can be customised but changing these defines  *****/
+/*
+ * Device configuration option defines.  Build can be customised buy changing and adding defines here
+ *
+ * Note, we chec if they are already defined in Makefile
+ */
 
-/* Audio Class Version.  Note we run in FS when in Audio Class 1.0 mode */
+/* Enable/Disable MIDI - Default is MIDI off */
+#ifndef MIDI
+#define MIDI               (0)
+#endif
+
+/* Enable/Disable SPDIF - Default is SPDIF off */
+#ifndef SPDIF
+#define SPDIF              (1)
+#endif
+
+/* Audio Class Version.  Note we run in FS when in Audio Class 1.0 mode. Default is 2 */
 //:Functionality
-/* Enable Fall-back to audio class 1.0 a FS */
-#define AUDIO_CLASS (2)
+#ifndef AUDIO_CLASS
+#define AUDIO_CLASS        (2)
+#endif
 
 /* Enable Fall-back to audio class 1.0 when connected to FS hub */
-#define AUDIO_CLASS_FALLBACK 1
+#ifndef AUDIO_CLASS_FALLBACK
+#define AUDIO_CLASS_FALLBACK (1)
+#endif
 //:
 
 /* Defines relating to channel count and channel arrangement (Set to 0 for disable) */ 
 //:audio_defs
-/* Number of USB streaming channels */
-#define NUM_USB_CHAN_IN   (2)         /* Device to Host */
-#define NUM_USB_CHAN_OUT  (2)         /* Host to Device */
-
-/* Enable S/PDIF output */
-//#define SPDIF         0  // In makefile
-
-/* Enable DFU interface, Note, requires a driver for Windows */
-#define DFU           1
-
-/* Disable MIDI */
-//#define MIDI          1  // In makefile
+/* Number of USB streaming channels - Default is 2 in 2 out */
+#ifndef NUM_USB_CHAN_IN
+#define NUM_USB_CHAN_IN    (2)         /* Device to Host */
+#endif
+#ifndef NUM_USB_CHAN_OUT
+#define NUM_USB_CHAN_OUT   (2)         /* Host to Device */
+#endif
 
 /* Number of IS2 chans to DAC..*/
-#define I2S_CHANS_DAC     (2)
+#ifndef I2S_CHANS_DAC
+#define I2S_CHANS_DAC      (2)
+#endif
 
 /* Number of I2S chans from ADC */
-#define I2S_CHANS_ADC     (2)
+#ifndef I2S_CHANS_ADC
+#define I2S_CHANS_ADC      (2)
+#endif
+
+/* Enable DFU interface, Note, requires a driver for Windows */
+#define DFU                (1)
 
 /* Master clock defines (in Hz) */
-#define MCLK_441          (256*44100)   /* 44.1, 88.2 etc */
-#define MCLK_48           (512*48000)   /* 48, 96 etc */
+#define MCLK_441           (256*44100)   /* 44.1, 88.2 etc */
+#define MCLK_48            (512*48000)   /* 48, 96 etc */
 
 /* Maximum frequency device runs at */
-#define MAX_FREQ                    (192000)       
+#define MAX_FREQ           (192000)       
 
 /* Index of SPDIF TX channel (duplicated DAC channels 1/2) */
-#define SPDIF_TX_INDEX              (0)
+#define SPDIF_TX_INDEX     (0)
 
 /* Default frequency device reports as running at */
 /* Audio Class 1.0 friendly freq */       
-#define DEFAULT_FREQ                (48000)            
+#define DEFAULT_FREQ       (48000)            
 //:
 /***** Defines relating to USB descriptors etc *****/
 //:usb_defs
-#define VENDOR_ID   (0x20B1) /* XMOS VID */
-#define PID_AUDIO_2 (0x0002) /* L1 USB Audio Reference Design PID */
-#define PID_AUDIO_1 (0x0003) /* L1 USB Audio Reference Design PID */
-#define BCD_DEVICE  (0x0600) /* Device release number in BCD: 0xJJMN
+#define VENDOR_ID          (0x20B1) /* XMOS VID */
+#define PID_AUDIO_2        (0x0002) /* L1 USB Audio Reference Design PID */
+#define PID_AUDIO_1        (0x0003) /* L1 USB Audio Reference Design PID */
+#define BCD_DEVICE         (0x0600) /* Device release number in BCD: 0xJJMN
                               * JJ: Major, M: Minor, N: Sub-minor */
 //:
 
-#define HOST_ACTIVE_CALL 1
+/* Enables HID EP */
+#define HID_CONTROLS       (1)
 
+/* Enables calls to hostactive functions */
+#define HOST_ACTIVE_CALL   (1)
 
 /* Define to Enable use of custom flash device for DFU interface */
 #if defined(DFU_CUSTOM_FLASH_DEVICE) && (DFU_CUSTOM_FLASH_DEVICE==0)
