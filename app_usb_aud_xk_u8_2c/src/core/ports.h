bcdf1da4cd70e4d09be1c667f19c4784b48c830d
Ross Owen <ross@xmos.com>
2013-06-20 15:37:11 +0100

    4 bit port removed.  DAC/ADC rst now used from 32A

--- app_usb_aud_xk_u8_2c/src/core/ports.h
+++ app_usb_aud_xk_u8_2c/src/core/ports.h
@@ -12,11 +12,7 @@ on stdcore[0] : out port p_gpo  = XS1_PORT_32A;
 /* Input port for buttons and switch */
 on stdcore[0] : in port p_sw    = XS1_PORT_4D;
 
-/* Output port for DAC/ADC reset lines, also has DSD Mode */
-on stdcore[0] : out port p_audrst = XS1_PORT_4C;
-
 /* I2C ports */
 on stdcore[0] : struct r_i2c i2cPorts = {XS1_PORT_1C, XS1_PORT_1G}; /* In a struct to use module_i2c_simple */
-
 
 #endif
