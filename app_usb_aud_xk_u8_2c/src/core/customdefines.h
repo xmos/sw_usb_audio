c77b1ca5e57e4d8f88092bd37e330ecca5f18f15
Sam Chesney <samc@xmos.com>
2013-12-04 19:01:52 +0000

    Moved iAP defines from customdefines.h to iAP_conf.h

--- app_usb_aud_xk_u8_2c/src/core/customdefines.h
+++ app_usb_aud_xk_u8_2c/src/core/customdefines.h
@@ -85,10 +85,6 @@
 
 #define SELF_POWERED 1
 
-/***** Defines relating to iOS identification *****/
-#define ACCESSORY_MODEL_NUMBER      "XK-USB-AUDIO-U8-2C"
-#define ACCESSORY_HARDWARE_VERSION  {1, 1, 0}
-
 /***** Defines relating to USB descriptors etc *****/
 //:usb_defs
 #define VENDOR_ID          (0x20B1) /* XMOS VID */
--- app_usb_aud_xk_u8_2c/src/extensions/mfi/iAP_conf.h
+++ app_usb_aud_xk_u8_2c/src/extensions/mfi/iAP_conf.h
@@ -1,4 +1,8 @@
 #include "devicedefines.h"
 
-#define IAP1 0
-#define IAP2 1
+#define IAP1 1
+#define IAP2 0
+
+/***** Defines relating to iOS identification *****/
+#define ACCESSORY_MODEL_NUMBER      "XK-USB-AUDIO-U8-2C"
+#define ACCESSORY_HARDWARE_VERSION  {1, 1, 0}
