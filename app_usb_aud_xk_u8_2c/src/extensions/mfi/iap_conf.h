5cbe4bd129b2bf3c9505bbe655904b4336495f35
Sam Chesney <samc@xmos.com>
2013-12-18 23:26:37 +0000

    Updated MFi firmware version defines (to allow iAP1 and iAP2 to construct version info in required format) Added iAP charge current defines

--- app_usb_aud_xk_u8_2c/src/extensions/mfi/iap_conf.h
+++ app_usb_aud_xk_u8_2c/src/extensions/mfi/iap_conf.h
@@ -2,8 +2,14 @@
 #define IAP2 0
 
 /***** Defines relating to iOS identification *****/
-#define ACCESSORY_MODEL_NUMBER      "XK-USB-AUDIO-U8-2C"
-#define ACCESSORY_HARDWARE_VERSION  {1, 1, 0}
+#define ACCESSORY_MODEL_NUMBER   "XK-USB-AUDIO-U8-2C"
+
+#define ACCESSORY_HARDWARE_MAJOR 1
+#define ACCESSORY_HARDWARE_MINOR 1
+#define ACCESSORY_HARDWARE_POINT 0
+
+#define ACCESSORY_CURRENT_SUPPLY 2400 // mA
+#define APPLE_BATTERY_CHARGING   1
 
 /* Include global device configuration defines */
 #include "devicedefines.h"
