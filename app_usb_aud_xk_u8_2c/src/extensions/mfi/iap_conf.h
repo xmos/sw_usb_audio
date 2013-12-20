da69bfe090bd1e751154b101a267d18378b68c93
Sam Chesney <samc@xmos.com>
2013-12-20 14:47:58 +0000

    iAP2 enabled on MFA board MFA HW rev number updated

--- app_usb_aud_xk_u8_2c/src/extensions/mfi/iap_conf.h
+++ app_usb_aud_xk_u8_2c/src/extensions/mfi/iap_conf.h
@@ -1,11 +1,11 @@
 #define IAP1 1
-#define IAP2 0
+#define IAP2 1
 
 /***** Defines relating to iOS identification *****/
 #define ACCESSORY_MODEL_NUMBER   "XK-USB-AUDIO-U8-2C"
 
 #define ACCESSORY_HARDWARE_MAJOR 1
-#define ACCESSORY_HARDWARE_MINOR 1
+#define ACCESSORY_HARDWARE_MINOR 2
 #define ACCESSORY_HARDWARE_POINT 0
 
 #define ACCESSORY_CURRENT_SUPPLY 2400 // mA
