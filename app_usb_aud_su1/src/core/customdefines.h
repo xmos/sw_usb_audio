4d2ff96b538d7ff5b376d18fb396d550d170ab34
Sam Chesney <samc@xmos.com>
2014-05-14 15:25:03 +0100

    xpd: Patched version number

--- app_usb_aud_su1/src/core/customdefines.h
+++ app_usb_aud_su1/src/core/customdefines.h
@@ -8,8 +8,8 @@
 #define _CUSTOMDEFINES_H_
 
 #define XMOS_USB_AUD_APP_MAJOR_VERSION 6
-#define XMOS_USB_AUD_APP_MINOR_VERSION 5
-#define XMOS_USB_AUD_APP_POINT_VERSION 1
+#define XMOS_USB_AUD_APP_MINOR_VERSION 6
+#define XMOS_USB_AUD_APP_POINT_VERSION 0
 
 /***** Device configuration option defines.  Build can be customised but changing these defines  *****/
 
