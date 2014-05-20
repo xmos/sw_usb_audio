0ea304114cd583ff9be3dce2fd0fa80171d1d5be
Ross Owen <ross@xmos.com>
2014-05-20 13:47:43 +0100

    Tidy of u16 with audio 8 board defines

--- app_usb_aud_skc_u16_audio8/src/extensions/cs5368.h
+++ app_usb_aud_skc_u16_audio8/src/extensions/cs5368.h
@@ -5,23 +5,11 @@
 #define CS5368_I2C_ADDR      0x4C
 
 //Register Addresess
-#define CS5368_CHIP_REV      0x00
 #define CS5368_GCTL_MDE      0x01
-#define CS5368_OVFL_ST       0x02
 #define CS5368_OVFL_MSK      0x03
 #define CS5368_HPF_CTRL      0x04
 #define CS5368_PWR_DN        0x06
 #define CS5368_MUTE_CTRL     0x08
 #define CS5368_SDO_EN        0x0a
-
-//Default values used in this application
-#define CS5368_CHIP_REV_VAL  0x81 //CS5368 Rev B of the chip
-#define CS5368_GCTL_MDE_VAL  0x97 //Enable control port, /2 mclk, I2S format, slave mode autodetect speed
-#define CS5368_OVFL_ST_VAL   0xff //No overflows (read only)
-#define CS5368_OVFL_MSK_VAL  0xff //enable all overflow interrupts
-#define CS5368_HPF_CTRL_VAL  0x00 //disable HPF
-#define CS5368_PWR_DN_VAL    0x00 //Everything powered up
-#define CS5368_MUTE_CTRL_VAL 0x00 //Mute off all channels
-#define CS5368_SDO_EN_VAL    0x00 //SDO pins enabled
 
 #endif /* _CS5368_H_ */
