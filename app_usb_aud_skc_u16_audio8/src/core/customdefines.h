79635ff907cd07bf38cdbd50076eeff17bb71e0d
Ross Owen <ross@xmos.com>
2014-06-02 11:27:58 +0100

    U16 + Audio8 customdefines.h tidy

--- app_usb_aud_skc_u16_audio8/src/core/customdefines.h
+++ app_usb_aud_skc_u16_audio8/src/core/customdefines.h
@@ -14,43 +14,41 @@
  */
 
 /* Tile on which audio IO is located */
-#define AUDIO_IO_TILE 1
-#define IAP_TILE 0
+#define AUDIO_IO_TILE      1
+
+/* Tile on which iAP must be located */
+#define IAP_TILE           0
 
 /* Enable/Disable MIDI - Default is MIDI on */
 #ifndef MIDI
-#define MIDI 		0
+#error
+#define MIDI 		       0
 #endif
 
 /* Enable/Disable SPDIF output - Default is S/PDIF on */
 #ifndef SPDIF
-#define SPDIF		1
+#define SPDIF		       1
 #endif
 
 /* Defines relating to channel count and channel arrangement (Set to 0 for disable) */
 //:audio_defs
 /* Number of USB streaming channels */
 #ifndef NUM_USB_CHAN_IN
-#define NUM_USB_CHAN_IN   (8)         /* Device to Host */
+#define NUM_USB_CHAN_IN    (10)         /* Device to Host */
 #endif
 #ifndef NUM_USB_CHAN_OUT
-#define NUM_USB_CHAN_OUT  (8)         /* Host to Device */
+#define NUM_USB_CHAN_OUT   (10)         /* Host to Device */
 #endif
 
 /* Number of I2S chans from ADC */
 #ifndef I2S_CHANS_ADC
-#define I2S_CHANS_ADC     (8)
+#define I2S_CHANS_ADC      (8)
 #endif
 
 /* Number of IS2 chans to DAC..*/
 #ifndef I2S_CHANS_DAC
-#define I2S_CHANS_DAC     (8)
+#define I2S_CHANS_DAC      (8)
 #endif
-
-/* Number of DSD chans to DAC..*/
-//#define DSD_CHANS_DAC     (2)
-
-#define MIDI_SHIFT_TX      7
 
 /* Master clock defines (in Hz) */
 #define MCLK_441          (512*44100)   /* 44.1, 88.2 etc */
@@ -70,11 +68,11 @@
 #define SELF_POWERED       1
 
 /* Index of SPDIF TX channel (duplicated DAC channels 1/2 when index is 0) */
-#define SPDIF_TX_INDEX              (0)
+#define SPDIF_TX_INDEX     (8)
 
 /***** Defines relating to USB descriptors etc *****/
-#define PID_AUDIO_2 (0x000C) /* XMOS U16 - Audio Class 2.0 */
-#define PID_AUDIO_1 (0x000D) /* XMOS U16 - Audio Class 1.0 */
+#define PID_AUDIO_2        (0x000C) /* XMOS U16 - Audio Class 2.0 */
+#define PID_AUDIO_1        (0x000D) /* XMOS U16 - Audio Class 1.0 */
 
 /* Enable/Disable example HID code */
 #define HID_CONTROLS       0
