57cef7693da46a1ca19ee0d876bf7949ebd9c4c4
Sam Chesney <samc@xmos.com>
2014-07-30 19:10:12 +0100

    Ensure USER_MAIN_CORES functions can be found

--- app_usb_aud_skc_u16_audio8/src/extensions/user_main.h
+++ app_usb_aud_skc_u16_audio8/src/extensions/user_main.h
@@ -1,6 +1,10 @@
 #ifndef _USER_MAIN_H_
 #define _USER_MAIN_H_
 
+#include <xccompat.h>
+
+void ea_protocol_demo(chanend c_ea_data);
+
 #define USER_MAIN_CORES \
     on tile[1] : ea_protocol_demo(c_ea_data);
 
