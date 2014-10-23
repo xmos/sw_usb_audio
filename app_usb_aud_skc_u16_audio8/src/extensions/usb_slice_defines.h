c6107080d6c02ff8eaba7af79acb960aac15256d
Ross Owen <ross@xmos.com>
2014-10-23 12:43:06 +0100

    Post merge build issues resolved

--- app_usb_aud_skc_u16_audio8/src/extensions/usb_slice_defines.h
+++ app_usb_aud_skc_u16_audio8/src/extensions/usb_slice_defines.h
@@ -1,8 +1,16 @@
 
+#include <xs1.h>
+
 /* Port/bit defs for USB slice on U16 core board */
+
+#define PORT_USB_GPIO            XS1_PORT_4C
+#define PORT_ACC_PWR             XS1_PORT_1A
+#define PORT_VBUS_OUT_EN         XS1_PORT_1D
+
+
 #define USB_SLICE_P4C_USB_SEL    0x01
 #define USB_SLICE_P4C_CPR_RST_N  0x08
 
-#define USB_SLICE_P1A_ACC_DET_EN 0x01
+//#define USB_SLICE_P1A_ACC_DET_EN 0x01
 
-#define USB_SLICE_P1D_VBUS_OUT_EN 0x01
+//#define USB_SLICE_P1D_VBUS_OUT_EN 0x01
