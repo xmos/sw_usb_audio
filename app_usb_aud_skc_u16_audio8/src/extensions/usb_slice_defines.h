7864f4b0927cf23232cdc5a8973fe45bc5cc8103
Ross Owen <ross@xmos.com>
2014-10-31 11:08:24 +0000

    removed unused defines.

--- app_usb_aud_skc_u16_audio8/src/extensions/usb_slice_defines.h
+++ app_usb_aud_skc_u16_audio8/src/extensions/usb_slice_defines.h
@@ -7,10 +7,6 @@
 #define PORT_ACC_PWR             XS1_PORT_1A
 #define PORT_VBUS_OUT_EN         XS1_PORT_1D
 
-
 #define USB_SLICE_P4C_USB_SEL    0x01
 #define USB_SLICE_P4C_CPR_RST_N  0x08
 
-//#define USB_SLICE_P1A_ACC_DET_EN 0x01
-
-//#define USB_SLICE_P1D_VBUS_OUT_EN 0x01
