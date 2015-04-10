1444e5beb722081a22bda97054369a7fb51c9c0a
Ross Owen <ross@xmos.com>
2015-04-10 12:54:23 +0100

    set_gpio() moved to gpio_access.c

--- app_usb_aud_x200/src/extensions/gpio_access.c
+++ app_usb_aud_x200/src/extensions/gpio_access.c
@@ -37,3 +37,14 @@ void p_gpio_out(unsigned x)
     // Wrapped in lock to ensure it's safe from multiple logical cores
     swlock_release(&gpo_swlock);
 }
+
+void set_gpio(unsigned bit, unsigned value)
+{
+	unsigned port_shadow;
+	port_shadow = p_gpio_peek();         // Read port pin value
+	if (value == 0) port_shadow &= ~bit; // If writing a 0, generate mask and AND with current val
+	else port_shadow |= bit;             // Else use mask and OR to set bit
+	p_gpio_out(port_shadow);             // Write back to port. Will make port an output if not already
+}
+
+
