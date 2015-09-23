40da0d9acfefc6b2cadfaf95a0309fd5a4877cc0
Ross Owen <ross@xmos.com>
2015-09-23 16:05:34 +0100

    PDM demo now 8 channels recording via USB.

--- app_usb_aud_exp_pdm/src/core/customdefines.h
+++ app_usb_aud_exp_pdm/src/core/customdefines.h
@@ -51,7 +51,7 @@
 //:audio_defs
 /* Number of USB streaming channels - Default is 4 in 4 out */
 #ifndef NUM_USB_CHAN_IN
-#define NUM_USB_CHAN_IN    (2)         /* Device to Host */
+#define NUM_USB_CHAN_IN    (8)         /* Device to Host */
 #endif
 #ifndef NUM_USB_CHAN_OUT
 #define NUM_USB_CHAN_OUT   (2)         /* Host to Device */
