0394a6e13e5798db406c5da9354f98e25b5b1611
Sam Chesney <samc@xmos.com>
2015-11-27 11:44:56 +0000

    Add test to check bcdDevice defines match version

--- tests/runtests.py
+++ tests/runtests.py
@@ -43,7 +43,7 @@ The tests are run at sample rates of 44.1kHz, 48kHz, 88.2kHz, 96kHz, 176.4kHz an
 
     xmostest.register_group("sw_usb_audio",
                             "non_audio_hw_tests",
-                            "A collection of tests covering non audio related features",
+                            "A collection of tests covering non audio related features run on hardware",
     """
 Tests are performed by running the USB Audio reference design software on
 XMOS audio hardware platforms. The audio platform under test will be connected
@@ -71,6 +71,16 @@ and record audio and other data.  Tests are run to test the following features:
 
 The tests are run at a sample rates of 192kHz. Tests utilise all audio channels
 of the board simultaneously.
+""")
+
+    xmostest.register_group("sw_usb_audio",
+                            "sw_release_tests",
+                            "A collection of tests covering software release checks",
+    """
+Tests are performed by inspecting the USB Audio reference design source code.
+Tests are run to check the following features:
+
+    * USB descriptor bcdDevice definition
 """)
 
     xmostest.runtests()
