b17b30e14143ad1f939a88694714f0fcddf61adf
Krishna Balan <krishnab@xmos.com>
2015-12-10 14:05:52 +0000

    Included OSX dev for the test runs

--- tests/test_volume_output.py
+++ tests/test_volume_output.py
@@ -372,7 +372,7 @@ def runtest():
 
     args = xmostest.getargs()
 
-    host_oss = ['os_x_11', 'win_7', 'win_8', 'win_10']
+    host_oss = ['os_x_11', 'os_x_dev', 'win_7', 'win_8', 'win_10']
 
     for test in test_configs:
         board = test['board']
