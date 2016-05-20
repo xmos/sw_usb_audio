c925c7c1806f4a23b7faf3f5b16da19e9612f9cf
Ross Owen <ross@xmos.com>
2016-05-20 17:34:48 +0100

    dsp interface moved to USER_MAIN_DECLARATIONS

--- app_usb_aud_mic_array/src/extensions/usermain.h
+++ app_usb_aud_mic_array/src/extensions/usermain.h
@@ -4,19 +4,14 @@
 
 #ifndef __STDC__
 #include "xua_audio.h"
-#include "xua_dsp.h"
+#include "xvsm_dsp.h"
 
 [[combinable]]
 void dsp_control(client dsp_ctrl_if i_dsp_ctrl);
-//void dsp_control(client dsp_ctrl_if i_dsp_ctrl);
-
-/* TODO move me? */
 void dsp_buff(server audManage_if i_manAud, client dsp_if i_dsp);
 
 /* Prototype for our custom genclock() task */
 void genclock();
-
-// TODO User NUM_DSP_CTRL_INTS
 
 #ifndef XVSM 
     #define USER_MAIN_CORES \
@@ -24,16 +19,21 @@ void genclock();
             on tile[PDM_TILE].core[0]: user_pdm_process(i_mic_process); 
 #else
 
+    #define USER_MAIN_DECLARATIONS  \
+        interface dsp_if i_dsp; \
+        interface dsp_ctrl_if i_dsp_ctrl[1]; // TODO NUM_DSP_CTRL_INTERFACES
+
+
     #ifdef MIC_PROCESSING_USE_INTERFACE
         #define USER_MAIN_CORES \
-            on tile[1] : genclock(); \
+            on tile[1]: genclock(); \
             on tile[PDM_TILE]: dsp_process(i_dsp, i_dsp_ctrl, 1); \
             on tile[PDM_TILE]: dsp_buff(i_audMan, i_dsp); \
             on tile[PDM_TILE]: dsp_control(i_dsp_ctrl[0]); \
             on tile[PDM_TILE].core[0]: user_pdm_process(i_mic_process); 
     #else
         #define USER_MAIN_CORES \
-            on tile[1] : genclock(); \
+            on tile[1]: genclock(); \
             on tile[PDM_TILE]: dsp_process(i_dsp, i_dsp_ctrl, 1); \
             on tile[PDM_TILE]: dsp_buff(i_audMan, i_dsp); \
             on tile[PDM_TILE]: dsp_control(i_dsp_ctrl[0]); 
