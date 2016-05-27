f098687a64aa2b5a7c591223a699625a3cb3431b
Ross Owen <ross@xmos.com>
2016-05-27 15:18:37 +0100

    Initial work to integrate control to app_usb_aud_mic_array using lib_control and new VENDOR_REQUESTS_PARAMS_XX defines

--- app_usb_aud_mic_array/src/extensions/usermain.h
+++ app_usb_aud_mic_array/src/extensions/usermain.h
@@ -2,13 +2,14 @@
 #ifndef _USERCODE_H_
 #define _USERCODE_H_
 
-#ifndef __STDC__
-#include "xua_audio.h"
-#include "xvsm_dsp.h"
+#ifdef XVSM
+    #define VENDOR_REQUESTS_PARAMS     i_modules
+    #define VENDOR_REQUESTS_PARAMS_DEC CLIENT_INTERFACE(control, i_modules[1])      /* TODO NUM_MODULES */
+#endif
 
-[[combinable]]
-void dsp_control(client dsp_ctrl_if i_dsp_ctrl);
-void dsp_buff(server audManage_if i_manAud, client dsp_if i_dsp);
+#ifndef __STDC__
+
+#include "xua_audio.h"
 
 /* Prototype for our custom genclock() task */
 void genclock();
@@ -19,9 +20,17 @@ void genclock();
             on tile[PDM_TILE].core[0]: user_pdm_process(i_mic_process); 
 #else
 
+    #include "xvsm_dsp.h"
+    #include "control.h"
+
+    [[combinable]]
+    void dsp_control(client dsp_ctrl_if i_dsp_ctrl);
+    void dsp_buff(server audManage_if i_manAud, client dsp_if i_dsp);
+
     #define USER_MAIN_DECLARATIONS  \
         interface dsp_if i_dsp; \
-        interface dsp_ctrl_if i_dsp_ctrl[1]; // TODO NUM_DSP_CTRL_INTERFACES
+        interface dsp_ctrl_if i_dsp_ctrl[1]; /* TODO NUM_DSP_CTRL_INTERFACES */ \
+        interface control i_modules[1];      /* TODO NUM_MODULES */
 
 
     #ifdef MIC_PROCESSING_USE_INTERFACE
