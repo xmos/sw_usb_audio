bd4a62f2675cfbd9a6239fdf1befcad3bf82aa7e
Ross Owen <ross@xmos.com>
2016-06-24 16:23:06 +0100

    Added VAD control resource ID base

--- app_usb_aud_mic_array/src/extensions/control_ids.h
+++ app_usb_aud_mic_array/src/extensions/control_ids.h
@@ -1,9 +1,11 @@
-#include "ivl_control.h"
+#include "xvsm_control.h"
+#include "xvsm_vad.h"
 #include "lib_voice_doa_naive.h"
 
 
 #define IVL_RESID_BASE 1
 #define DOA_RESID_BASE (IVL_RESID_BASE + IVL_RESID_COUNT)
+#define VAD_RESID_BASE (DOA_RESID_BASE + DOA_RESID_COUNT)
 
 
 
