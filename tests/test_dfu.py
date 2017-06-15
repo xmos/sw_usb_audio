08b28038089598166ab2e4c2e6e3c087a0eeb176
Sam Chesney <samc@xmos.com>
2017-06-15 17:49:31 +0100

    Update DFU test to use script in sc_usb_audio

--- tests/test_dfu.py
+++ tests/test_dfu.py
@@ -263,10 +263,10 @@ def do_dfu_test(min_testlevel, board, app_name, pid, grep_string, app_config, ho
 
     for os in host_oss:
         if os.startswith('os_x'):
-            host_dfu_path = "../../../../usb_audio_testing/dfu/OSX/testdfu.sh"
+            host_dfu_path = "../../../../sc_usb_audio/module_dfu/host/xmos_dfu_osx/testdfu.sh"
             dfu_args = ["%s" % host_dfu_path, pid, grep_string, "<remote_file>", "<remote_file>"]
         elif os.startswith('win_'):
-            host_dfu_path = '"..\\..\\..\\..\\usb_audio_testing\\dfu\\win32\\testdfu.bat "C:\\Program Files\\Thesycon\\TUSBAudio_Driver\\dfucons.exe""'
+            host_dfu_path = '"..\\..\\..\\..\\sc_usb_audio\\module_dfu\\host\\windows\\testdfu.bat "C:\\Program Files\\Thesycon\\TUSBAudio_Driver\\dfucons.exe""'
             dfu_args = ["%s" % host_dfu_path, "<remote_file>", "<remote_file>"]
         dfu_job[os] = xmostest.run_on_pc(resources[os]['host'], dfu_args,
                                      files_used_as_args = ['%s/upgrade1.bin' % dut_app_path, '%s/upgrade2.bin' % dut_app_path],
