0bd5476564fce3310e0b950da4ed422cee8ccb56
Sam Chesney <samc@xmos.com>
2017-08-11 15:02:22 +0100

    Fix upgrade binary creation during DFU test
    The second upgrade image should also be marked as "upgrade 1" because
    the flash is always layed out as:
    | factory | upgrade |
    and not:
    | factory | upgrade | upgrade |

--- tests/test_dfu.py
+++ tests/test_dfu.py
@@ -240,7 +240,7 @@ def do_dfu_test(min_testlevel, board, app_name, pid, grep_string, app_config, ho
                            'xflash --factory-version 14.3 --upgrade 1 bin/%s/%s_%s.xe -o upgrade1.bin --verbose;' % (app_config, app_name, app_config),
                            'xmake clean CONFIG=%s;' % app_config,
                            'xmake CONFIG=%s TEST_DFU_2=1;' % app_config,
-                           'xflash --factory-version 14.3 --upgrade 2 bin/%s/%s_%s.xe -o upgrade2.bin --verbose' % (app_config, app_name, app_config)]
+                           'xflash --factory-version 14.3 --upgrade 1 bin/%s/%s_%s.xe -o upgrade2.bin --verbose' % (app_config, app_name, app_config)]
             cmd_string = " ".join([x for x in xflash_cmds])
         else:
             cmds = ['echo "Building upgrade images in other process"',]
