2980b458575c941041f126ecb870a3140a30d439
xross <ross@xmos.com>
2022-05-06 17:01:56 +0100

    Fix for DFU on XK-EVK-XU316

--- app_usb_aud_xk_evk_xu316/src/core/xua_defs.h
+++ app_usb_aud_xk_evk_xu316/src/core/xua_defs.h
@@ -83,7 +83,7 @@
 #define HID_CONTROLS       (0)
 #endif
 
-
+# if 0
 /* Flash part for xcore-AI explorer board: MX25R3235FM1IH0 32MBIT */
 #define FL_QUADDEVICE_MX25R3235 \
 { \
@@ -111,9 +111,40 @@
     0x01,                   /* QSPI_WRSR */ \
     0x01,                   /* QSPI_WIP_BIT_MASK */ \
 }
-
 // DFU_FLASH_DEVICE is a comma-separated list of flash spec structures
 // This define is used in sc_usb_audio/module_usb_audio/flashlib_user.c
 #define DFU_FLASH_DEVICE FL_QUADDEVICE_MX25R3235
+#else
+#define FL_QUADDEVICE_AT25FF321A \
+{ \
+    0,                      /* UNKNOWN */ \
+    256,                    /* page size */ \
+    16384,                  /* num pages */ \
+    3,                      /* address size */ \
+    3,                      /* log2 clock divider */ \
+    0x9F,                   /* QSPI_RDID */ \
+    0,                      /* id dummy bytes */ \
+    3,                      /* id size in bytes */ \
+    0x1F4708,               /* device id */ \
+    0x20,                   /* QSPI_SE */ \
+    4096,                   /* Sector erase is always 4KB */ \
+    0x06,                   /* QSPI_WREN */ \
+    0x04,                   /* QSPI_WRDI */ \
+    PROT_TYPE_SR,           /* Protection via SR */ \
+    {{0x3C,0x00},{0,0}},    /* QSPI_SP, QSPI_SU */ \
+    0x02,                   /* QSPI_PP */ \
+    0xEB,                   /* QSPI_READ_FAST */ \
+    1,                      /* 1 read dummy byte */ \
+    SECTOR_LAYOUT_REGULAR,  /* mad sectors */ \
+    {4096,{0,{0}}},         /* regular sector sizes */ \
+    0x05,                   /* QSPI_RDSR */ \
+    0x01,                   /* QSPI_WRSR */ \
+    0x01,                   /* QSPI_WIP_BIT_MASK */ \
+}
+
+#endif
+// DFU_FLASH_DEVICE is a comma-separated list of flash spec structures
+// This define is used in sc_usb_audio/module_usb_audio/flashlib_user.c
+#define DFU_FLASH_DEVICE FL_QUADDEVICE_AT25FF321A
 
 #endif
