800d9bec50f9124607a5280d823a203909f41edf
xross <ross@xmos.com>
2022-11-01 17:59:33 +0000

    Removed old define

--- app_usb_aud_xk_216_mc/src/core/app_usb_aud_xk_216_mc.h
+++ app_usb_aud_xk_216_mc/src/core/app_usb_aud_xk_216_mc.h
@@ -1,22 +1,10 @@
 // Copyright 2018-2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
-/**
- * @file       app_vf_spk_base.h
- * @brief      Defines relating to device configuration and customisation.
- *             For xCORE-200 Microphone Array board (2vx)
- * @author     Ross Owen, XMOS Limited
- */
-
 #ifndef APP_USB_AUD_XK_216_MC_H_
 #define APP_USB_AUD_XK_216_MC_H_
 
 #include "xua_conf.h"
-
-/* Default to board version version 2.0 */
-#ifndef XCORE_200_MC_AUDIO_HW_VERSION
-#define XCORE_200_MC_AUDIO_HW_VERSION 2
-#endif
 
 #ifndef USB_SEL_A
 #define USB_SEL_A    (0)
