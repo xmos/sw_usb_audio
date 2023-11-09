170aebbc24c803a0987a264114ade98663fa9b2b
Daniel Pieczko <danielpieczko@xmos.com>
2023-11-09 16:29:01 +0000

    Add support for XCommon CMake

---
+++ app_usb_aud_xk_216_mc/configs_partial.cmake
@@ -0,0 +1,46 @@
+#
+# Configs that have been partially tested.
+#
+
+# Enabling "build" test configs implicitly enables "partial" test configs
+if(BUILD_TESTED_CONFIGS)
+    set(PARTIAL_TESTED_CONFIGS TRUE)
+endif()
+
+
+if(PARTIAL_TESTED_CONFIGS)
+
+set(APP_COMPILER_FLAGS_2AMi8o8xxxxxx ${SW_USB_AUDIO_FLAGS})
+
+set(APP_COMPILER_FLAGS_2ASi8o8xxxxxx_tdm8 ${SW_USB_AUDIO_FLAGS} -DXUA_PCM_FORMAT=XUA_PCM_FORMAT_TDM
+                                                                -DMAX_FREQ=96000 -DCODEC_MASTER=1
+                                                                -DOUT_VOLUME_IN_MIXER=0)
+
+set(APP_COMPILER_FLAGS_2ASi8o8xxxxxx ${SW_USB_AUDIO_FLAGS} -DCODEC_MASTER=1)
+
+set(APP_COMPILER_FLAGS_2AMi8o10xxsxxx ${SW_USB_AUDIO_FLAGS} -DXUA_SPDIF_TX_EN=1)
+
+set(APP_COMPILER_FLAGS_2AMi8o10mxsxxx ${SW_USB_AUDIO_FLAGS} -DMIDI=1
+                                                            -DXUA_SPDIF_TX_EN=1)
+
+set(APP_COMPILER_FLAGS_2AMi8o8xxxxxd ${SW_USB_AUDIO_FLAGS} -DDSD_CHANS_DAC=2)
+
+set(APP_COMPILER_FLAGS_2AMi8o10xxsxxx_mix8 ${SW_USB_AUDIO_FLAGS} -DXUA_SPDIF_TX_EN=1
+                                                                 -DMAX_MIX_COUNT=8)
+
+set(APP_COMPILER_FLAGS_2AMi8o10xxsxxd ${SW_USB_AUDIO_FLAGS} -DXUA_SPDIF_TX_EN=1
+                                                            -DDSD_CHANS_DAC=2)
+
+set(APP_COMPILER_FLAGS_2AMi16o8xxxaxx ${SW_USB_AUDIO_FLAGS} -DXUA_ADAT_RX_EN=1
+                                                            -DMAX_FREQ=96000)
+
+set(APP_COMPILER_FLAGS_2SMi8o8xxxxxx ${SW_USB_AUDIO_FLAGS} -DXUA_SYNCMODE=XUA_SYNCMODE_SYNC)
+
+set(APP_COMPILER_FLAGS_2AMi8o16xxxxax ${SW_USB_AUDIO_FLAGS} -DXUA_ADAT_TX_EN=1
+                                                            -DMAX_FREQ=96000)
+
+set(APP_COMPILER_FLAGS_2AMi16o16xxxaax ${SW_USB_AUDIO_FLAGS} -DXUA_ADAT_RX_EN=1
+                                                             -DXUA_ADAT_TX_EN=1
+                                                             -DMAX_FREQ=96000)
+
+endif()
---
+++ app_usb_aud_xk_316_mc/configs_build.cmake
@@ -0,0 +1,30 @@
+#
+# Configs that have only had their build process tested
+#
+if(BUILD_TESTED_CONFIGS)
+
+# Audio Class 2, Sync, I2S Master, 10xInput, 10xOutput, TDM
+# (1024x Mclk required for 192/176 TDM)
+set(APP_COMPILER_FLAGS_2SMi10o10xxxxxx_tdm8 ${SW_USB_AUDIO_FLAGS} -DXUA_PCM_FORMAT=XUA_PCM_FORMAT_TDM
+                                                                  -DXUA_SYNCMODE=XUA_SYNCMODE_SYNC
+                                                                  -DMCLK_48=1024*48000
+                                                                  -DMCLK_441=1024*44100)
+
+# Audio Class 1, Adaptive, I2S Master, 8xInput, 8xOutput, TDM
+set(APP_COMPILER_FLAGS_1DMi8o8xxxxxx ${SW_USB_AUDIO_FLAGS} -DXUA_PCM_FORMAT=XUA_PCM_FORMAT_TDM
+                                                           -DXUA_SYNCMODE=XUA_SYNCMODE_ADAPT
+                                                           -DAUDIO_CLASS=1)
+
+# Audio Class 2, Async, I2S Master, 8xInput, 8xOutput, Mixer (mixer does volume processing after mix)
+set(APP_COMPILER_FLAGS_2AMi8o8xxxxxx_mix8_vol_after ${SW_USB_AUDIO_FLAGS} -DMAX_MIX_COUNT=8
+                                                                          -DOUT_VOLUME_IN_MIXER=1
+                                                                          -DIN_VOLUME_IN_MIXER=1)
+
+
+# Audio Class 2, Async, I2S Master, 8xInput, 8xOutput, Mixer (mixer does volume processing before mix)
+set(APP_COMPILER_FLAGS_2AMi8o8xxxxxx_mix8_vol_before ${SW_USB_AUDIO_FLAGS} -DMAX_MIX_COUNT=8
+                                                                           -DOUT_VOLUME_IN_MIXER=1
+                                                                           -DIN_VOLUME_IN_MIXER=1
+                                                                           -DOUT_VOLUME_AFTER_MIX=0
+                                                                           -DIN_VOLUME_AFTER_MIX=0)
+endif()
