d62601b9d412c29ce11fe5440afd2a3b04d10f7e
Daniel Pieczko <danielpieczko@xmos.com>
2023-11-13 15:12:06 +0000

    Fix errors in build config definitions

--- app_usb_aud_xk_216_mc/configs_build.cmake
+++ app_usb_aud_xk_216_mc/configs_build.cmake
@@ -3,7 +3,7 @@
 #
 if(BUILD_TESTED_CONFIGS)
 
-set(APP_COMPILER_FLAGS_1AMi8o2xxxxxx ${SW_USB_AUDIO_FLAGS} -DAUDIO_CLASS=1
+set(APP_COMPILER_FLAGS_1AMi6o2xxxxxx ${SW_USB_AUDIO_FLAGS} -DAUDIO_CLASS=1
                                                            -DNUM_USB_CHAN_IN_FS=6
                                                            -DMAX_FREQ_FS=44100
                                                            -DSTREAM_FORMAT_INPUT_1_RESOLUTION_BITS=16)
@@ -34,7 +34,7 @@ set(APP_COMPILER_FLAGS_2AMi16o16xxxxxx_tdm8 ${SW_USB_AUDIO_FLAGS} -DI2S_CHANS_DA
 set(APP_COMPILER_FLAGS_2AMi32o32xxsxxx_tdm8 ${SW_USB_AUDIO_FLAGS} -DI2S_CHANS_ADC=32
                                                                   -DI2S_CHANS_DAC=32
                                                                   -DNUM_USB_CHAN_IN=32
-                                                                  -DNUM_USB_CHAN_OUT=16
+                                                                  -DNUM_USB_CHAN_OUT=32
                                                                   -DXUA_PCM_FORMAT=XUA_PCM_FORMAT_TDM
                                                                   -DMAX_FREQ=48000
                                                                   -DXUA_SPDIF_TX_EN=1
