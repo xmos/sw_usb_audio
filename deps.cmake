f662b68193fc186911dbc1908627468a0cfeabb8
Ed <ed@xmos.com>
2024-01-23 13:26:38 +0000

    re-run on previous xua commit

--- deps.cmake
+++ deps.cmake
@@ -1,2 +1,2 @@
-set(APP_DEPENDENT_MODULES "ed-xmos/lib_xua(feature/sw_pll_sync_ua)"
+set(APP_DEPENDENT_MODULES "ed-xmos/lib_xua(44049ecfca7799d2cde6b4c814cea0c378a91ae4)"
                           "lib_i2c")
