43d8b946a42b404b97a05935b5b900cd6b654faa
Ed <ed@xmos.com>
2024-04-25 13:17:51 +0100

    Move wait for ports to earlier to ensure audio device enum'd too

--- tests/test_midi_stress.py
+++ tests/test_midi_stress.py
@@ -71,46 +71,45 @@ def test_midi_loopback_stress(pytestconfig, board, config):
     duration = midi_duration(pytestconfig.getoption("level"), features["partial"])
     fail_str = ""
 
-    # fs_audio = [max(features["samp_freqs"])] # Highest rate for maximum stress
-    fs_audio = [min(features["samp_freqs"])]
+    fs_audio = [max(features["samp_freqs"])] # Highest rate for maximum stress
     with XrunDut(adapter_dut, board, config) as dut:
-            with (
-                AudioAnalyzerHarness(
-                    adapter_harness, xscope="io"
-                ) as harness,
-                # Due to in and out in xsig config this works in both directions
-                XsigInput(fs_audio, duration, xsig_config_path, dut.dev_name, ident=f"analogue_input-{board}-{config}-{fs_audio}") as xsig_proc_in
-                ):
 
-                # # Ensure firmware is up and enumerated as MIDI
-                # wait_for_midi_ports()
-                
-                # with (mido.open_input(find_xmos_midi_device(mido.get_input_names())) as in_port,
-                #       mido.open_output(find_xmos_midi_device(mido.get_output_names())) as out_port):
-                
-                #     # Keep looping midi_test until time up
-                #     while time.time() < time_start + duration + xsig_completion_time_s:
-                #         run_midi_test(input_midi_file_name, output_midi_file_name, in_port, out_port)
+        # Ensure firmware is up and enumerated as MIDI
+        wait_for_midi_ports()
 
-                time.sleep(duration + xsig_completion_time_s)
+        time_start = time.time()
+        with (
+            AudioAnalyzerHarness(
+                adapter_harness, xscope="io"
+            ) as harness,
+            # Due to in and out in xsig config this streams audio in both directions for max stress
+            XsigInput(fs_audio, duration, xsig_config_path, dut.dev_name, ident=f"analogue_input-{board}-{config}-{fs_audio}") as xsig_proc_in
+            ):
+            
+            with (mido.open_input(find_xmos_midi_device(mido.get_input_names())) as in_port,
+                  mido.open_output(find_xmos_midi_device(mido.get_output_names())) as out_port):
+            
+                # Keep looping midi_test until time up
+                while time.time() < time_start + duration + xsig_completion_time_s:
+                    run_midi_test(input_midi_file_name, output_midi_file_name, in_port, out_port)
 
-            # Stop the harness
-            harness.terminate()
-            xscope_lines = harness.get_output() # This will always see a loss of signal at the end but useful for debug
-            xsig_lines = xsig_proc_in.get_output()
+        # Stop the harness
+        harness.terminate()
+        xscope_lines = harness.get_output() # This will always see a loss of signal at the end but useful for debug
+        xsig_lines = xsig_proc_in.get_output()
 
-            with open(xsig_config_path) as file:
-                xsig_json = json.load(file)
+        with open(xsig_config_path) as file:
+            xsig_json = json.load(file)
 
-            # xsig outout parsing for D2H streaming
-            failures = check_analyzer_output(xsig_lines, xsig_json["in"])
-            if len(failures) > 0:
-                fail_str += f"Failure at sample rate {fs_audio}\n"
-                fail_str += "\n".join(failures) + "\n\n"
-                fail_str += f"xscope stdout at sample rate {fs_audio}\n"
-                fail_str += "\n".join(xscope_lines) + "\n\n"
-                fail_str += f"xsig stdout at sample rate {fs_audio}\n"
-                fail_str += "\n".join(xsig_lines) + "\n\n"
+        # xsig outout parsing for D2H streaming
+        failures = check_analyzer_output(xsig_lines, xsig_json["in"])
+        if len(failures) > 0:
+            fail_str += f"Failure at sample rate {fs_audio}\n"
+            fail_str += "\n".join(failures) + "\n\n"
+            fail_str += f"xscope stdout at sample rate {fs_audio}\n"
+            fail_str += "\n".join(xscope_lines) + "\n\n"
+            fail_str += f"xsig stdout at sample rate {fs_audio}\n"
+            fail_str += "\n".join(xsig_lines) + "\n\n"
 
     if len(fail_str) > 0:
         print(fail_str)
