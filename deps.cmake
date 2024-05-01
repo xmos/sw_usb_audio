56be1e841d567040a28eb603642d7fc346ea2929
Ed <ed@xmos.com>
2024-05-01 11:22:41 +0100

    Update deps to fetch from Ed's fork

--- deps.cmake
+++ deps.cmake
@@ -1,3 +1,3 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "ed-xmos/lib_xua(feature/correct_ep_for_in_only)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
