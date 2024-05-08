1db74d8537da187051b6ac3e0e46b60ba4eff456
Shuchita Khare <shuchitak@xmos.com>
2024-05-08 13:02:07 +0100

    Use lib_xua branch with the IN buffer underflow fix in decoupler

--- deps.cmake
+++ deps.cmake
@@ -1,3 +1,3 @@
-set(APP_DEPENDENT_MODULES "lib_xua(4.0.0)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(bugfix/in_buffer_underflow)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
