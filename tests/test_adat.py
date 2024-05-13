0d3ccbfd685b1dab383550c51d76aa326636298f
Shuchita Khare <shuchitak@xmos.com>
2024-05-13 12:11:32 +0100

    Run adat tests for longer duration

--- tests/test_adat.py
+++ tests/test_adat.py
@@ -61,6 +61,7 @@ def adat_output_uncollect(pytestconfig, board, config):
 
 
 def adat_duration(level, partial):
+    return 180
     if level == "weekend":
         duration = 90 if partial else 1200
     elif level == "nightly":
