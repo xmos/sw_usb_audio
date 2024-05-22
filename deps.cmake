9d87dde0291b287e88b6487ba1c246c43add6427
Shuchita Khare <shuchitak@xmos.com>
2024-05-22 11:07:24 +0100

    test with lib_xua develop

--- deps.cmake
+++ deps.cmake
@@ -1,3 +1,3 @@
-set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(fix/alt_itf_out_chan_count)"
+set(APP_DEPENDENT_MODULES "lib_xua(develop)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
