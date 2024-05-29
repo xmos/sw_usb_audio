933cfaddb272a95d91f1fda1dac59938d4bf8b9d
Shuchita Khare <shuchitak@xmos.com>
2024-05-29 16:29:47 +0100

    use lib_xua(develop)

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
 set(APP_DEPENDENT_MODULES "lib_sw_pll(develop)"
-                          "shuchitak/lib_xua(midi_ct_pause)"
+                          "lib_xua(develop)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
