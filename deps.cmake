8da315c83f32910b7f7c70a3802168c7172d8412
Shuchita Khare <shuchitak@xmos.com>
2024-05-29 14:00:03 +0100

    add i18o18mssaax config

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
 set(APP_DEPENDENT_MODULES "lib_sw_pll(develop)"
-                          "lib_xua(develop)"
+                          "shuchitak/lib_xua(midi_ct_pause)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
