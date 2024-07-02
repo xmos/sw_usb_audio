6f4e2805e1a4f2550b37dcac1d0607762f78e45c
Shuchita Khare <shuchitak@xmos.com>
2024-07-02 10:03:41 +0100

    use head of develop of xua

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,3 @@
-set(APP_DEPENDENT_MODULES "lib_spdif(spdif_transmit_inline)"
-                          "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "lib_xua(develop)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)")
