eab5b03c19998f985793cb3b58d70045bea42bc2
Shuchita Khare <shuchitak@xmos.com>
2024-08-08 10:41:02 +0100

    revert to lib_xua develop

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(dfu_1_1)"
+set(APP_DEPENDENT_MODULES "lib_xua(develop)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)"
                           "lib_board_support(0.1.1)")
