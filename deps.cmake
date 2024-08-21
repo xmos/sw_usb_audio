31404d744aaf06cad42af72fde34947c413518cc
Shuchita Khare <shuchitak@xmos.com>
2024-08-21 11:44:14 +0100

    test xua uac1 dfu branch

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(test_dfu_uac1)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)"
                           "lib_board_support(0.1.1)")
