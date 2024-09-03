64a45ff2a6062e2e141b0cabae2d6e3bfc2abf0d
Ed <ed@xmos.com>
2024-09-03 10:49:27 +0100

    Try XN move branch of BSP

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
 set(APP_DEPENDENT_MODULES "lib_xua(develop)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)"
-                          "lib_board_support(develop)")
+                          "lib_board_support(feature/move_xns)")
