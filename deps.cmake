dffe8f45dfb3ce4f6f69e154f4e8b676a90f75af
Shuchita Khare <shuchitak@xmos.com>
2024-10-15 10:24:16 +0100

    Test the latest adat release

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(update_adat)"
                           "lib_i2c(6.2.0)"
                           "lib_i2s(5.1.0)"
                           "lib_board_support(0.1.1)")
