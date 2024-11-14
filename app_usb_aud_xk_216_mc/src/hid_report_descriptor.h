70680618a09b6cd50a48af7bf5449888166f5c90
Ross Owen <ross@xmos.com>
2024-11-14 12:04:46 +0000

    Fixed bad copyright comment date

--- app_usb_aud_xk_216_mc/src/hid_report_descriptor.h
+++ app_usb_aud_xk_216_mc/src/hid_report_descriptor.h
@@ -1,4 +1,4 @@
-// Copyright 2022 XMOS LIMITED.
+// Copyright 2022-2024 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #ifndef _hid_report_descriptor_h_
 #define _hid_report_descriptor_h_
