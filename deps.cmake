b38ac11daa588b74bd43fd09a10b7fe032d496fd
Daniel Pieczko <danielpieczko@xmos.com>
2024-12-05 10:50:09 +0000

    Test with lib_xud branch high_priority_mode

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,5 @@
-set(APP_DEPENDENT_MODULES "lib_xua(5.0.0)"
+set(APP_DEPENDENT_MODULES "danielpieczko/lib_xud(high_priority_mode)"
+                          "lib_xua(5.0.0)"
                           "lib_i2c(6.4.0)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(1.1.1)")
