598d91f71efebe7bc6e7ee2416d38b1871db9821
Ed <ed@xmos.com>
2025-04-30 16:55:42 +0100

    Use ed's XUA fork for LP non-streaming

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "ed-xmos/lib_xua(feature/no_stream_power_simpl)"
                           "lib_i2c(6.4.0)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(1.2.2)")
