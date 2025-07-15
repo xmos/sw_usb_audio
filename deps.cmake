0e624b2b4554b945bb516039108fab6a85dd78e9
shuchitak <38428600+shuchitak@users.noreply.github.com>
2025-07-15 15:43:20 +0100

    Update deps.cmake

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(bugfix/mclk_for_dig_rx)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(bugfix/negative_ferror)"
                           "lib_i2c(6.4.0)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(1.3.0)")
