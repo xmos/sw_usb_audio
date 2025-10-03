b976c39766ac8db6990738612a7aa99adc18d0dc
shuchitak <38428600+shuchitak@users.noreply.github.com>
2025-10-03 14:52:32 +0100

    Update APP_DEPENDENT_MODULES in deps.cmake

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(more_mixer)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(spdif_tx_sample_length)"
                           "lib_i2c(6.4.0)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(1.3.0)")
