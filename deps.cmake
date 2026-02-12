f07271c5b8a31bd9211631637b5066581e7ed47c
Shuchita Khare <shuchitak@xmos.com>
2026-02-12 16:40:27 +0000

    test shuchitak/lib_xua(release/5.3.0)

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(release/5.3.0)"
                           "lib_i2c(6.4.1)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(develop)")
