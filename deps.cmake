b5a53e7583227d645126de77eb8655495dc935a1
shuchitak <38428600+shuchitak@users.noreply.github.com>
2026-03-30 15:02:34 +0100

    test shuchitak/lib_xua(bugfix/fs_feedback_lsb)

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
-set(APP_DEPENDENT_MODULES "lib_xua(develop)"
+set(APP_DEPENDENT_MODULES "shuchitak/lib_xua(bugfix/fs_feedback_lsb)"
                           "lib_i2c(6.4.1)"
                           "lib_i2s(6.0.1)"
                           "lib_board_support(develop)")
